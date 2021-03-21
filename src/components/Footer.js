8d8d954810fb379052a113a482ba5e40b6ea4059
Louise <louisepeiffer@hotmail.com>
2021-03-21 23:14:29 +0100

    fini i think

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,18 +6,20 @@ export default function Footer() {
             <div className="footer1">
                 <div>
                     <h1>Social Media</h1>
-                    <a href=""><i className="fab fa-facebook-f"></i></a>
-                    <a href=""><i className="fab fa-twitter"></i></a>
-                    <a href=""><i className="fab fa-instagram"></i></a>
-                    <a href=""><i className="fab fa-youtube"></i></a>
+                    <a href="https://www.facebook.com" target="_blank"><i className="fab fa-facebook-f"></i></a>
+                    <a href="https://www.twitter.com" target="_blank"><i className="fab fa-twitter"></i></a>
+                    <a href="https://www.instagram.com" target="_blank"><i className="fab fa-instagram"></i></a>
                 </div>
                 <div>
-                    <h1>Envoyez moi un email</h1>
+                    <h1>Contactez-moi</h1>
                     <a href=""><i class="fas fa-envelope-open-text"></i></a>
+                    <a href="tel:+2222222"><i class="fas fa-phone-square-alt"></i></a>
+
                 </div>
                 <div>
-                    <h1>On s'appelle</h1>
-                    <a href=""><i class="fas fa-phone-square-alt"></i></a>
+                    <h1>Motivation quote</h1>
+                    <p>“Yeah I’m in my bag but I’m in his too. That’s why every time you see me I got some new shoes.”</p>
+                    <span>– Megan Thee Stallion, “Cash Sh*t”</span>
                 </div>
             </div>
             <div className="footer2">
